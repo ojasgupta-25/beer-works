8d7c7cc2505a3fdf40dfa84b3ebc609c779a9401
ojasgupta-25 <ojasgupta25@gmail.com>
2023-06-19 08:41:30 -0500

    added spring files

---
+++ src/main/java/ojasbeerproject/beerworks/BeerWorksApplication.java
@@ -0,0 +1,13 @@
+package ojasbeerproject.beerworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ojasbeerproject/beerworks/BeerWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package ojasbeerproject.beerworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
